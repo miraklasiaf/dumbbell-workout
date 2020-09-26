969461b9d4f9c70e7950131c9cd5064ea24a4b14
Faisal Karim <faisalkarim96@gmail.com>
2020-09-26 14:45:29 +0700

    use img tag

--- components/video-card.js
+++ components/video-card.js
@@ -1,4 +1,4 @@
-import { Link, Flex, Box, Stack, Heading, Text, Image } from '@chakra-ui/core'
+import { Link, Flex, Box, Stack, Heading, Text } from '@chakra-ui/core'
 
 const VideoCard = ({ title, length, src, href, children, ...rest }) => (
   <Link
@@ -12,13 +12,13 @@ const VideoCard = ({ title, length, src, href, children, ...rest }) => (
     {...rest}
   >
     <Flex direction={['column', 'column', 'row']}>
-      <Image
+      <Box
+        as="img"
         w="250px"
         h="150px"
         mr={8}
         objectFit="cover"
         src={src}
-        fallbackSrc="https://via.placeholder.com/150"
         borderRadius={8}
         boxShadow="0px 20px 40px rgba(0, 0, 0, 0.2)"
       />
